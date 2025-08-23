ccdfe05a29cca65241bb224ad89d26db362997fa
Hannah Henderson <hannahhenderson@Hannahs-MacBook-Pro.local>
2025-08-23 15:01:30 -0400

    Add some additional gratitude

--- src/run.ts
+++ src/run.ts
@@ -1,4 +1,23 @@
 import { cp0through100 } from "./crackle-pop";
 
-console.log("Thank you for taking the time to review my application!");
+console.log(String.raw`
+  _____ _                 _     __     __
+ |_   _| |               | |    \ \   / /
+   | | | |__   __ _ _ __ | | __  \ \_/ /__  _   _
+   | | | '_ \ / _\` | '_ \| |/ /   \   / _ \| | | |
+   | | | | | | (_| | | | |   <     | | (_) | |_| |
+   \_/ |_| |_|\__,_|_| |_|_|\_\    |_|\___/ \__,_|
+
+
+Thank you for taking the time to review my application!
+
+
+                     *********** 
+
+
+`);
+
+console.log(`The CracklePop function run from 0 to 100, inclusive:
+    `);
+
 cp0through100();
